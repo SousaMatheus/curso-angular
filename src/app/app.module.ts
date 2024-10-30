f34c734c77ad79ec70735d3e49d7e237dbd485df
Matheus Sousa <ma.sousa.soares@gmail.com>
2024-10-29 23:01:02 -0300

    Adiciona componente `two-way-binding`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { EmitterFilhoComponent } from './components/emitter-filho/emitter-filho.
 import { HostPaiComponent } from './components/host-pai/host-pai.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
 import { PipeOperatorComponent } from './components/pipe-operator/pipe-operator.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
 
 @NgModule({
   declarations: [
@@ -25,8 +27,9 @@ import { PipeOperatorComponent } from './components/pipe-operator/pipe-operator.
     HostPaiComponent,
     ListRenderComponent,
     PipeOperatorComponent,
+    TwoWayBindingComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
